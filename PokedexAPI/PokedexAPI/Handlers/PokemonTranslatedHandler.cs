b84c833c2235b4909124955cf72618de722c739e
danjones457 <danjonesy@ymail.com>
2022-04-05 22:17:31 +0100

    Create pokemon translated handler
    - Created pokemon translated handler
    - Updated GetTranslatedPokemon to return an untranslated pokemon
    - This currently does the same as the standard pokemon endpoint

---
+++ PokedexAPI/PokedexAPI/Handlers/PokemonTranslatedHandler.cs
@@ -0,0 +1,27 @@
+﻿using PokedexAPI.Interfaces.Handlers;
+using PokedexAPI.Models;
+
+namespace PokedexAPI.Handlers
+{
+    public class PokemonTranslatedHandler : IPokemonTranslatedHandler
+    {
+        private readonly ILogger<PokemonTranslatedHandler> _logger;
+        private readonly IPokemonHandler _pokemonHandler;
+
+        public PokemonTranslatedHandler(ILogger<PokemonTranslatedHandler> logger, IPokemonHandler pokemonHandler)
+        {
+            _logger = logger;
+            _pokemonHandler = pokemonHandler;
+        }
+
+        /// <summary>
+        /// See <see cref="IPokemonTranslatedHandler.GetTranslatedPokemon(string)"/>
+        /// </summary>
+        /// <param name="pokemon"></param>
+        /// <returns></returns>
+        public async Task<Pokemon> GetTranslatedPokemon(string pokemon)
+        {
+            return await _pokemonHandler.GetPokemon(pokemon);
+        }
+    }
+}
