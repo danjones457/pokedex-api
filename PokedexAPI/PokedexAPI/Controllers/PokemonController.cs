1a4da91d7ff316ac30544252b9d91125680a4151
danjones457 <danjonesy@ymail.com>
2022-04-05 20:36:03 +0100

    Pokemon endpoint for valid pokemon
    - Added the logic to fetch the pokemon data
    - This does not currently handle invalid pokemon passed to the API

--- PokedexAPI/PokedexAPI/Controllers/PokemonController.cs
+++ PokedexAPI/PokedexAPI/Controllers/PokemonController.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json.Linq;
 using PokedexAPI.Models;
 
 namespace PokedexAPI.Controllers
@@ -8,17 +9,54 @@ namespace PokedexAPI.Controllers
     public class PokemonController : ControllerBase
     {
         private readonly ILogger<PokemonController> _logger;
+        private readonly IConfiguration _configuration;
 
-        public PokemonController(ILogger<PokemonController> logger)
+        public PokemonController(ILogger<PokemonController> logger, IConfiguration configuration)
         {
             _logger = logger;
+            _configuration = configuration;
         }
 
         [HttpGet]
         [Route("{pokemon}")]
-        public Pokemon Get(string pokemon)
+        public async Task<Pokemon> GetAsync(string pokemon)
         {
-            return new Pokemon();
+            using var client = new HttpClient();
+            var pokeApiUrl = _configuration["PokeApiUrl"];
+
+            try
+            {
+                var pokeApiResponse = await client.GetStringAsync(pokeApiUrl + "/pokemon-species/" + pokemon);
+
+                var formattedResponse = JObject.Parse(pokeApiResponse);
+
+                var habitat = formattedResponse.SelectToken("habitat.name").Value<string>();
+                var isLegendary = formattedResponse.SelectToken("is_legendary").Value<bool>();
+
+                var descriptions = formattedResponse.SelectToken("flavor_text_entries").Value<JArray>();
+                var descriptionsList = descriptions.ToObject<List<JObject>>();
+                var enDescriptionObject = descriptionsList.FirstOrDefault(description => description.SelectToken("language.name").Value<string>() == "en");
+                var enDescription = "";
+
+                if (enDescriptionObject != null)
+                {
+                    enDescription = enDescriptionObject.Value<string>("flavor_text");
+                    enDescription = enDescription.Replace("\n", " ");
+                    enDescription = enDescription.Replace("\f", " ");
+                }
+
+                return new Pokemon
+                {
+                    Name = pokemon,
+                    Description = enDescription,
+                    Habitat = habitat,
+                    IsLegendary = isLegendary,
+                };
+            }
+            catch (Exception ex)
+            {
+                throw;
+            }
         }
     }
 }
