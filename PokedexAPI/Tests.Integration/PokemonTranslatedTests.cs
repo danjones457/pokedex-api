58b9c13e4c65644e7beefdfab11d5e6b898d160f
danjones457 <danjonesy@ymail.com>
2022-04-05 17:29:05 +0100

    Pokemon translated tests
    - Added tests to test the pokemon translated endpoint
    - Added successful Yoda translation calls
    - Added successful Shakespeare translation calls
    - Added invalid pokemon translation call

---
+++ PokedexAPI/Tests.Integration/PokemonTranslatedTests.cs
@@ -0,0 +1,77 @@
+using Newtonsoft.Json;
+using PokedexAPI.Models;
+using System.Net.Http;
+using Xunit;
+
+namespace Tests.Integration
+{
+    public class PokemonTranslatedTests
+    {
+        private readonly HttpClient Client;
+
+        public PokemonTranslatedTests()
+        {
+            var server = new MockPokedexApi();
+            Client = server.CreateClient();
+        }
+
+        [Theory]
+        [InlineData(
+            "mewtwo",
+            "Created by a scientist after years of horrific gene splicing and dna engineering experiments, it was.",
+            "rare",
+            true)]
+        [InlineData(
+            "zubat",
+            "Forms colonies in perpetually dark places.Ultrasonic waves to identify and approach targets, uses.",
+            "cave",
+            false)]
+        [InlineData(
+            "registeel",
+            "A body that is harder than any kind of metal, registeel has.Apparently hollow, its body is.Any idea what this pokémon eats, no one has.",
+            "cave",
+            true)]
+        public async void Test_Get_Valid_Translated_Pokemon_For_Yoda_Translation_Is_Successful(string name, string description, string habitat, bool isLegendary)
+        {
+            var response = await Client.GetAsync("/pokemon/" + name);
+
+            response.EnsureSuccessStatusCode();
+
+            var formattedResponse = await response.Content.ReadAsStringAsync();
+
+            var pokemonResponse = JsonConvert.DeserializeObject<Pokemon>(formattedResponse);
+
+            Assert.Equal(name, pokemonResponse.Name);
+            Assert.Equal(description, pokemonResponse.Description);
+            Assert.Equal(habitat, pokemonResponse.Habitat);
+            Assert.Equal(isLegendary, pokemonResponse.IsLegendary);
+        }
+
+        [Theory]
+        [InlineData("ratta", "Bites aught at which hour 't attacks. Bawbling and very quick, 't is a ingraft sight in many places.", "grassland", false)]
+        [InlineData("caterpie", "Its short feet art tipped with suction pads yond enable 't to tirelessly climb slopes and walls. ", "forest", false)]
+        public async void Test_Get_Valid_Translated_Pokemon_For_Shakespeare_Translation_Is_Successful(string name, string description, string habitat, bool isLegendary)
+        {
+            var response = await Client.GetAsync("/pokemon/" + name);
+
+            response.EnsureSuccessStatusCode();
+
+            var formattedResponse = await response.Content.ReadAsStringAsync();
+
+            var pokemonResponse = JsonConvert.DeserializeObject<Pokemon>(formattedResponse);
+
+            Assert.Equal(name, pokemonResponse.Name);
+            Assert.Equal(description, pokemonResponse.Description);
+            Assert.Equal(habitat, pokemonResponse.Habitat);
+            Assert.Equal(isLegendary, pokemonResponse.IsLegendary);
+        }
+
+        [Fact]
+        public async void Test_Get_Invalid_Pokemon_Is_Not_Successful()
+        {
+            var response = await Client.GetAsync("/pokemon/translated/not-a-pokemon");
+
+            Assert.Equal(System.Net.HttpStatusCode.BadRequest, response.StatusCode);
+        }
+    }
+}
