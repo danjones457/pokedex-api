fa5cf237bc30ce31db7fc19e3b3e620082d4399d
danjones457 <danjonesy@ymail.com>
2022-04-06 19:53:56 +0100

    Start of PokeApiToPokemonHelper tests
    - Added tests for parameter values being null
    - Added a test for valid inputs

---
+++ PokedexAPI/Tests.Unit/Helpers/PokeApiToPokemonHelperTests.cs
@@ -0,0 +1,79 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using PokedexAPI.Helpers;
+using PokedexAPI.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Tests.Unit.Helpers
+{
+    public class PokeApiToPokemonHelperTests
+    {
+        private readonly PokeApiToPokemonHelper _helper;
+        public PokeApiToPokemonHelperTests()
+        {
+            _helper = new PokeApiToPokemonHelper();
+        }
+
+        [Fact]
+        public void PokeApiToPokemonHelper_Should_Throw_When_Pokemon_Is_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => _helper.ConvertPokeApiResponseToPokemon(null, "test string"));
+        }
+
+        [Fact]
+        public void PokeApiToPokemonHelper_Should_Throw_When_Response_Is_Null()
+        {
+            Assert.Throws<ArgumentNullException>(() => _helper.ConvertPokeApiResponseToPokemon("test string", null));
+        }
+
+        [Fact]
+        public void PokeApiToPokemonHelper_Should_Return_Pokemon_With_Valid_Parameters()
+        {
+            var pokemon = "test pokemon";
+            var habitat = "test habitat";
+            var isLegendary = true;
+            var description = "test description";
+
+            var expectedResult = new Pokemon
+            {
+                Name = pokemon,
+                Habitat = habitat,
+                Description = description,
+                IsLegendary = isLegendary
+            };
+
+            var flavor_text_entries = new List<object>
+            {
+                new { 
+                    flavor_text = description,
+                    language = new
+                    {
+                        name = "en"
+                    }
+                }
+            };
+            var response = new
+            {
+                habitat = new
+                {
+                    name = habitat
+                },
+                is_legendary = isLegendary,
+                flavor_text_entries = flavor_text_entries
+            };
+            var stringResponse = JsonConvert.SerializeObject(response);
+
+            var helperResponse = _helper.ConvertPokeApiResponseToPokemon(pokemon, stringResponse);
+
+            Assert.Equal(expectedResult.Name, helperResponse.Name);
+            Assert.Equal(expectedResult.Habitat, helperResponse.Habitat);
+            Assert.Equal(expectedResult.Description, helperResponse.Description);
+            Assert.Equal(expectedResult.IsLegendary, helperResponse.IsLegendary);
+        }
+    }
+}
